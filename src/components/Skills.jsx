d0d4addb887b9a5ee82808f9a7adbec25e5fed73
TSOlami <tijani.saheed@yahoo.com>
2023-09-15 21:29:31 +0100

    fix border colours

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,15 +9,15 @@ const Skills = () => {
         <div className="w-full">
           <SectionTitle title="SKILLS" subtitle="WHAT I DO" />
         </div>
-        <div className="xl:border-l-2 xl:border-r-2 xl:border-primary-400 h-full">
+        <div className="xl:border-l-2 xl:border-r-2 xl:border-secondary-200 h-full">
           <div className="relative">
-            <div className="absolute top-[45px] w-full h-[1px] bg-primary-400"></div>
+            <div className="absolute top-[45px] w-full h-[1px] bg-secondary-200"></div>
             <MarqueeCards direction="left">
               <SkillsCards />
             </MarqueeCards>
           </div>
           <div className="relative">
-            <div className="absolute top-[45px] w-full h-[1px] bg-primary-400"></div>
+            <div className="absolute top-[45px] w-full h-[1px] bg-secondary-200"></div>
             <MarqueeCards direction="right">
               <SkillsCards />
             </MarqueeCards>
