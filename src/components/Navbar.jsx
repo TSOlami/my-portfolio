0962e5b547cf5e420fa0447852069a7ed0366cea
TSOlami <tijani.saheed@yahoo.com>
2023-11-26 16:22:15 +0100

    Fix navbar background color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,17 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5
-    fixed top-0 z-20`}>
+    <nav
+    className={`${styles.paddingX} w-full flex items-center py-5
+    fixed top-0 z-20`}
+    style={{
+      backgroundImage:
+        "linear-gradient(to bottom, rgba(0, 0, 0, 0.2), transparent)"
+    }}>
 
-      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+      <div
+      className="w-full flex justify-between items-center max-w-7x1 mx-auto"
+      >
       <Link
           to="/"
           className="flex items-center gap-2"
@@ -51,7 +58,7 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } pt-20 p-6 absolute top-2 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } brown-gradient pt-20 p-6 absolute top-2 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className="list-none flex justify-end items-start flex-col gap-4">
               {navLinks.map((link) => {
