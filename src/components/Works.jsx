e2c419cb6d11caff42108f1e6b1a95020b41422a
TSOlami <tijani.saheed@yahoo.com>
2023-11-26 15:44:26 +0100

    Fix icon size and color

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -42,15 +42,15 @@ const ProjectCard = ({
               <img
                 src={github}
                 alt='source code'
-                className='w-1/2 h-1/2 object-contain'
+                className='w-2/3 h-2/3 object-contain'
               />
             </div>
           </div>
         </div>
 
         <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+          <h3 className='text-secondary-50 font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary-50 text-[14px]'>{description}</p>
         </div>
 
         <div className='mt-4 flex flex-wrap gap-2'>
@@ -67,7 +67,7 @@ const ProjectCard = ({
         <div className="mt-2 flex flex-wrap gap-1"></div>
 				<div className="mt-3 flex justify-center items-center">
 					<a
-						className="shadow-md shadow-primary p-2 bg-tertiary rounded-lg flex justify-center"
+						className="text-secondary-50 shadow-md shadow-primary p-2 bg-tertiary rounded-lg flex justify-center"
 						href={project_link}
 						target="_blank"
             rel="noreferrer"
@@ -84,22 +84,9 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-      <SectionTitle title="PROJECTS" subtitle="MY WORK" />
+      <SectionTitle title="PROJECTS" subtitle="WHAT I HAVE DONE SO FAR" />
       </motion.div>
-
-      <div className='w-full flex'>
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
-        >
-          The following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-        </motion.p>
-      </div>
-
+      
       <div className='mt-20 flex flex-wrap gap-7'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
