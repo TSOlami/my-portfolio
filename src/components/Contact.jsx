0f378fe90c2c0cf1b3ccb1381e5b91bcdfdf95ff
TSOlami <tijani.saheed@yahoo.com>
2023-11-26 15:44:06 +0100

    Fix text color

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -76,7 +76,7 @@ const Contact = () => {
         <SectionTitle title="" subtitle="Get in touch" />
         <h2
         style={{ fontFamily: "'Orbitron', san-serif"}}
-        className="tracking-wider text-5xl sm:text-6xl md:text-7xl"
+        className="text-secondary-50 tracking-wider text-5xl sm:text-6xl md:text-7xl"
         >
         <Typewriter words={["Contact."]} loop={true} />
         &nbsp;
@@ -87,45 +87,45 @@ const Contact = () => {
           className='mt-12 flex flex-col gap-8 font-montserrat'
         >
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+            <span className='text-secondary-50 font-medium mb-4'>Your Name</span>
             <input
               type='text'
               name='name'
               value={form.name}
               onChange={handleChange}
               placeholder="What's your name?"
-              className='bg-glass py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className='bg-glass py-4 px-6 placeholder:text-secondary-200 text-secondary-50 rounded-lg outline-none border-none font-medium'
               required
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+            <span className='text-secondary-50 font-medium mb-4'>Your email</span>
             <input
               type='email'
               name='email'
               value={form.email}
               onChange={handleChange}
               placeholder="What's your email address?"
-              className='bg-glass py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className='bg-glass py-4 px-6 placeholder:text-secondary-200 text-white rounded-lg outline-none border-none font-medium'
               required
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+            <span className='text-secondary-50 font-medium mb-4'>Your Message</span>
             <textarea
               rows={7}
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-glass py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              placeholder='What do you want to say?'
+              className='bg-glass py-4 px-6 placeholder:text-secondary-200 text-white rounded-lg outline-none border-none font-medium'
               required
             />
           </label>
 
           <button
             type='submit'
-            className='bg-glass py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-tertiary hover:shadow-none transition-all duration-300 ease-in-out'
+            className='bg-glass py-3 px-8 rounded-xl outline-none w-fit text-secondary-50 font-bold shadow-md shadow-tertiary hover:shadow-none transition-all duration-300 ease-in-out'
           >
             {loading ? "Sending..." : "Send"}
           </button>
