e614257973179e7d6c501b214fd50bab928a7098
TSOlami <tijani.saheed@yahoo.com>
2023-11-28 15:42:56 +0100

    Fix image and import issue

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,10 +16,6 @@ import {
 	git,
 	figma,
 	docker,
-	meta,
-	starbucks,
-	tesla,
-	shopify,
 	namssn,
 	trackc,
 	threejs,
@@ -35,6 +31,7 @@ import {
 	database,
 	c,
 	pyIcon,
+	whosteejay,
   } from "../assets";
 
   import {
@@ -150,87 +147,7 @@ import {
 	},
   ];
   
-  const experiences = [
-	{
-	  title: "React.js Developer",
-	  company_name: "Starbucks",
-	  icon: starbucks,
-	  iconBg: "#383E56",
-	  date: "March 2020 - April 2021",
-	  points: [
-		"Developing and maintaining web applications using React.js and other related technologies.",
-		"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-		"Implementing responsive design and ensuring cross-browser compatibility.",
-		"Participating in code reviews and providing constructive feedback to other developers.",
-	  ],
-	},
-	{
-	  title: "React Native Developer",
-	  company_name: "Tesla",
-	  icon: tesla,
-	  iconBg: "#E6DEDD",
-	  date: "Jan 2021 - Feb 2022",
-	  points: [
-		"Developing and maintaining web applications using React.js and other related technologies.",
-		"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-		"Implementing responsive design and ensuring cross-browser compatibility.",
-		"Participating in code reviews and providing constructive feedback to other developers.",
-	  ],
-	},
-	{
-	  title: "Web Developer",
-	  company_name: "Shopify",
-	  icon: shopify,
-	  iconBg: "#383E56",
-	  date: "Jan 2022 - Jan 2023",
-	  points: [
-		"Developing and maintaining web applications using React.js and other related technologies.",
-		"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-		"Implementing responsive design and ensuring cross-browser compatibility.",
-		"Participating in code reviews and providing constructive feedback to other developers.",
-	  ],
-	},
-	{
-	  title: "Full stack Developer",
-	  company_name: "Meta",
-	  icon: meta,
-	  iconBg: "#E6DEDD",
-	  date: "Jan 2023 - Present",
-	  points: [
-		"Developing and maintaining web applications using React.js and other related technologies.",
-		"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-		"Implementing responsive design and ensuring cross-browser compatibility.",
-		"Participating in code reviews and providing constructive feedback to other developers.",
-	  ],
-	},
-  ];
-  
-  const testimonials = [
-	{
-	  testimonial:
-		"I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-	  name: "Sara Lee",
-	  designation: "CFO",
-	  company: "Acme Co",
-	  image: "https://randomuser.me/api/portraits/women/4.jpg",
-	},
-	{
-	  testimonial:
-		"I've never met a web developer who truly cares about their clients' success like Rick does.",
-	  name: "Chris Brown",
-	  designation: "COO",
-	  company: "DEF Corp",
-	  image: "https://randomuser.me/api/portraits/men/5.jpg",
-	},
-	{
-	  testimonial:
-		"After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-	  name: "Lisa Wang",
-	  designation: "CTO",
-	  company: "456 Enterprises",
-	  image: "https://randomuser.me/api/portraits/women/6.jpg",
-	},
-  ];
+  const experiences = [];
   
   const education = [
 	{
@@ -433,7 +350,7 @@ import {
 				color: "pink-text-gradient",
 			},
 		],
-		image: avatar,
+		image: whosteejay,
 		source_code_link: "https://github.com/TSOlami/my-portfolio",
 		project_link: "https://whosteejay.netlify.app/"
 	}
@@ -451,4 +368,4 @@ import {
 	],
   };
 
-  export { services, technologies, experiences, testimonials, education, projects, introduction, memoji, skills };
+  export { services, technologies, experiences, education, projects, introduction, memoji, skills };
